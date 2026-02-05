691ababd90a38b26927cff87abcc8eb24ca7df33
dosclic98 <davidesavarro98@gmail.com>
2026-02-05 18:11:22 +0100

    Updated runContainer.sh and requirements.txt

--- runContainer.sh
+++ runContainer.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 mkdir -p dbn-sim-learning-container/{models,plots,results,traces}
 
+# Place the pysmile_license.py file in the current directory before running this command.
 sudo docker run --rm -it \
+  -v "$PWD/pysmile_license.py":/home/simulation/dbn-sim-learning/pysmile_license.py:ro \
   -v "$PWD/dbn-sim-learning-container/models":/home/simulation/dbn-sim-learning/models \
   -v "$PWD/dbn-sim-learning-container/plots":/home/simulation/dbn-sim-learning/plots \
   -v "$PWD/dbn-sim-learning-container/results":/home/simulation/dbn-sim-learning/results \
