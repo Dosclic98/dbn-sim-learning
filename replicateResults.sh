930abf2e0e163b9aa8ed6a5230a5bcbbc2dbf24e
dosclic98 <davidesavarro98@gmail.com>
2026-03-26 17:39:01 +0100

    Fixed replication script

--- replicateResults.sh
+++ replicateResults.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-$numProcsToUse = $(( $(nproc) - 2 ))
-$numProcsToUse = $(( $numProcsToUse > 0 ? $numProcsToUse : 1 ))
-$numRuns = 1000
+set -euo pipefail
+
+numProcsToUse=$(( $(nproc) - 2 ))
+numProcsToUse=$(( numProcsToUse > 0 ? numProcsToUse : 1 ))
+numRuns=1000
 echo "Executing $numRuns runs using at most $numProcsToUse parallel processes..."
-python3 replicate_results.py -r $numRuns -p $numProcsToUse
+python3 replicate_results.py -r "$numRuns" -p "$numProcsToUse"
